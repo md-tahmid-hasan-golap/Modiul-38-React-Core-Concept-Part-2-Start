a3728e446ee83c166062e668ba40b20c86fcaabb
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-08 15:29:31 +0600

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert("I am Clicked");
+  }
 
+  function clickedButton() {
+    alert("Click the button");
+  }
+
+  const arrowFunction = () => {
+    alert("Arrow function");
+  };
+
+  const arrowFunction2 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* amra java script a ai vabe event handlar diye kaj kori */}
+      {/* <button onClick="handleClick()">Click Me</button> */}
+
+      <button
+        style={{
+          marginBottom: "10px",
+        }}
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
+      <br />
+      <button onClick={clickedButton}>Button</button>
+
+      <button onClick={arrowFunction}>Arrow function</button>
+
+      <br />
+      <button
+        onClick={function golap() {
+          alert("Golap");
+        }}
+      >
+        Golap
+      </button>
+      <br />
+      <button onClick={() => arrowFunction2(5)}>Count</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
